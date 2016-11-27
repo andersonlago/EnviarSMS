41713cd721356dd3a73a6c178182185f85ffc185
anderson <andersonlagogomes@gmail.com>
2016-11-27 00:51:55 -0300

    organizacao

--- app/src/main/java/com/example/anderson/enviarsms/MainActivity.java
+++ app/src/main/java/com/example/anderson/enviarsms/MainActivity.java
@@ -20,12 +20,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-   }
+    }
 
-    public void onClick(View v){
+    public void onClick(View v) {
 
-        EditText txtnumero = (EditText) findViewById (R.id.txtnumero);
-        EditText txtmsg =  (EditText) findViewById( R.id.txtmsg );
+        EditText txtnumero = (EditText) findViewById(R.id.txtnumero);
+        EditText txtmsg = (EditText) findViewById(R.id.txtmsg);
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(txtnumero.getText().toString(), null, txtmsg.getText().toString(), null, null);
 
@@ -35,10 +35,10 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     @Override
     public void onInit(int status) {
 
-       tts.setLanguage( new Locale("pt_BR"));
-       EditText txtmsg =  (EditText) findViewById( R.id.txtmsg );
-       tts.speak(txtmsg.getText().toString(), TextToSpeech.QUEUE_ADD,null);
-  }
+        tts.setLanguage(new Locale("pt_BR"));
+        EditText txtmsg = (EditText) findViewById(R.id.txtmsg);
+        tts.speak(txtmsg.getText().toString(), TextToSpeech.QUEUE_ADD, null);
+    }
 }
 
 
